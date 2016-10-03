cbbaa79a72d670adbeb376d09a016954e94571f1
Redstone Zhao <13740080@qq.com>
2016-10-03 22:04:29 +0000

    提高 controller-home 的测试覆盖度到100%

---
+++ test/controller-home.js
@@ -0,0 +1,63 @@
+var assert    = require('assert')
+  , rest      = require('open-rest')
+  , U         = require('../build/app/lib/utils')
+  , home      = require('../build/app/controllers/home');
+
+describe('controllers home', function() {
+
+  describe('#index', function() {
+
+    it('req.username', function(done) {
+      var req = {
+        user: {
+          id: 1,
+          username: 'Redstone Zhao'
+        },
+        _clientIp: '192.168.199.188',
+        _realIp: '192.168.199.199',
+        _remoteIp: '127.0.0.1',
+        privateSwitchs: '*'
+      };
+      var res = {
+        send: function(data) {
+          assert.deepEqual([
+            'GET /users'
+          ], data[1]);
+        }
+      };
+      home.index(req, res, function(error) {
+        assert.equal(null, error);
+
+        done();
+      });
+    });
+
+    it('Guest', function(done) {
+      var req = {
+        user: {
+          id: 0
+        },
+        _clientIp: '192.168.199.188',
+        _realIp: '192.168.199.199',
+        _remoteIp: '127.0.0.1',
+        privateSwitchs: [
+          'users'
+        ]
+      };
+      var res = {
+        send: function(data) {
+          assert.deepEqual([
+            'GET /users'
+          ], data[1]);
+        }
+      };
+      home.index(req, res, function(error) {
+        assert.equal(null, error);
+
+        done();
+      });
+    });
+
+  });
+
+});
