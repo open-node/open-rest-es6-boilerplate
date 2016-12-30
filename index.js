5b77666ae6cb3304fbd6ce5b559487332e222329
Redstone Zhao <13740080@qq.com>
2016-12-30 11:42:49 +0000

    修改写法

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ const U = require('./app/lib/utils');
 const config = require('./app/configs');
 const routes = require('./app/routes');
 
-const requireModel = require;
 const cache = config.cache || {};
 U.cached.init(cache.port, cache.host, cache.opts);
 
-if (U.isProd) {
-  U.rest.utils.logger = U.logger = U.bunyan.createLogger(config.logger);
-}
+if (U.isProd) U.rest.utils.logger = U.logger = U.bunyan.createLogger(config.logger);
 
 // 初始化Model，且将获取Model定义的函数注册到 U 上面
 U.model = U.openRestWithMysql(U.rest, `${__dirname}/app/models`, config.db);
 
-const middleWares = requireModel('./app/middle-wares');
+const middleWares = require('./app/middle-wares');
+
 const controllers = U.getModules(`${__dirname}/app/controllers`, 'js');
 const service = config.service;
 
